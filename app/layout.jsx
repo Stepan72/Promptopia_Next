887e0ee611314145d8a06834e19afc8cce233c43
Stepan72 <st.nim@list.ru>
2023-05-09 16:35:27 +0600

    initial version

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+// import "@styles/global.css";
+import "./../styles/globals.css";
+import { Nav, Provider } from "@components";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover and share AI Prompts",
+};
+
+function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
---
+++ components/Provider.jsx
@@ -0,0 +1,7 @@
+import { SessionProvider } from "next-auth/react";
+
+function Provider({ children, session }) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
+
+export default Provider;
